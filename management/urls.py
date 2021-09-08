a12f594b0b25e72533db514634c049b0f6eeec12
Aalzate95 <alex.alzate95@gmail.com>
2021-09-07 22:37:15 -0500

    urls api

--- management/urls.py
+++ management/urls.py
@@ -5,8 +5,7 @@ from django.urls import path, include
 
 
 router = routers.DefaultRouter()
-router.register(r'solicitudesReserva',
-                views.SolicitudesReservaViewSet, basename='SolicitudesReserva')
+router.register(r'solicitudesReserva',views.SolicitudesReservaViewSet, basename='SolicitudesReserva')
 router.register(r'menu', views.MenuViewSet, basename='Menu')
 
 urlpatterns = [
